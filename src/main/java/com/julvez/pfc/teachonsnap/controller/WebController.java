21b4cd4a2959ba97b3e90dbf46050fa4e78851c9
julpfc <david.julvez@gmail.com>
2014-10-18 13:56:40 +0200

    Hola Mundo MVC

---
+++ src/main/java/com/julvez/pfc/teachonsnap/controller/WebController.java
@@ -0,0 +1,50 @@
+package com.julvez.pfc.teachonsnap.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.julvez.pfc.teachonsnap.model.Lesson;
+
+/**
+ * Servlet implementation class WebController
+ */
+public class WebController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WebController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession(true);
+	    // Set the session valid for 5 secs
+	    session.setMaxInactiveInterval(5);
+	    
+	    Lesson less = new Lesson();
+		less.setId(1);
+		less.setTitle(session.getId());
+				
+		request.setAttribute("lesson", less); 
+	    request.getRequestDispatcher("/WEB-INF/views/lesson.jsp").forward(request, response);	    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
