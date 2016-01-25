f87adc7f6ceecc04495401a3ca90d37a589c620e
julpfc <julpfc@users.noreply.github.com>
2016-01-25 17:56:28 +0100

    Javadoc tag prev

--- src/main/java/com/julvez/pfc/teachonsnap/tag/model/CloudTag.java
+++ src/main/java/com/julvez/pfc/teachonsnap/tag/model/CloudTag.java
@@ -3,49 +3,78 @@ package com.julvez.pfc.teachonsnap.tag.model;
 import com.julvez.pfc.teachonsnap.lesson.model.Lesson;
 import com.julvez.pfc.teachonsnap.user.model.User;
 
+/**
+ * Domain entity. Describes a tag to be part of a cloud of tags.
+ * Can be created from a Tag, an author (User) or a Lesson.
+ * It contains the tag's text, the relative weight at the cloud
+ * and a URL.
+ */
 public class CloudTag {
 	
+	/** Tag's text */
 	private String tag;
+	
+	/** Tag's URL */
 	private String URL;
+	
+	/** Tag's relative weight at the cloud */
 	private short weight;
 
-	public CloudTag(Tag tag,short weight){
+	/** 
+	 * Creates a new CloudTag from a Tag with this relative 
+	 * weight at the cloud.
+	 * @param tag CloudTag will be created from this Tag
+	 * @param weight Tag's relative weight at the cloud
+	 */
+	public CloudTag(Tag tag, short weight){
 		this.tag = tag.getTag();
 		this.URL = tag.getURL();
 		this.weight=weight;
 	}
 	
-	public CloudTag(User author,short weight){
+	/** 
+	 * Creates a new CloudTag from a User (author) with 
+	 * this relative weight at the cloud.
+	 * @param author CloudTag will be created from this User
+	 * @param weight Tag's relative weight at the cloud
+	 */
+	public CloudTag(User author, short weight){
 		this.tag = author.getFullName();
 		this.URL = author.getURL();
 		this.weight=weight;
 	}
 	
+	/** 
+	 * Creates a new CloudTag from a Lesson with this relative 
+	 * weight at the cloud.
+	 * @param lesson CloudTag will be created from this Lesson
+	 * @param weight Tag's relative weight at the cloud
+	 */
 	public CloudTag(Lesson lesson, short weight){
 		this.tag = lesson.getTitle();
 		this.URL = lesson.getURL();
 		this.weight = weight;
 	}
 	
+	/**
+	 * @return tag's text
+	 */
 	public String getTag() {
 		return tag;
 	}
-	public void setTag(String tag) {
-		this.tag = tag;
-	}
+	
+	/**
+	 * @return tag's relative weight at the cloud.
+	 */
 	public short getWeight() {
 		return weight;
 	}
-	public void setWeight(short weight) {
-		this.weight = weight;
-	}
-
+	
+	/**
+	 * @return tag's URL
+	 */
 	public String getURL() {
 		return URL;
-	}
-
-	public void setURL(String uRL) {
-		URL = uRL;
 	}
 
 	@Override
@@ -53,9 +82,4 @@ public class CloudTag {
 		return "CloudTag [tag=" + tag + ", URL=" + URL + ", weight=" + weight
 				+ "]";
 	}
-	
-	
-		
-	
-	
 }
