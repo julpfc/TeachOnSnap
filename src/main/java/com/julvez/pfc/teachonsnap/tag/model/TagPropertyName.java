fac17d3637e3bb22fcc24c89facdd2eb58e77a77
julpfc <julpfc@users.noreply.github.com>
2016-01-25 18:04:02 +0100

    javadoc tag prev

--- src/main/java/com/julvez/pfc/teachonsnap/tag/model/TagPropertyName.java
+++ src/main/java/com/julvez/pfc/teachonsnap/tag/model/TagPropertyName.java
@@ -1,10 +1,16 @@
 package com.julvez.pfc.teachonsnap.tag.model;
 
+/**
+ * Enumeration with the properties names related to tags.
+ * <p>
+ * To be used on the aplication properties file.
+ */
 public enum TagPropertyName {
 
+	/** Max number of tags in a cloud of tags. */
 	LIMIT_CLOUDTAG("tag.repository.limit.cloudtag");
 	
-	 		
+	/** Real property name on the properties file */ 		
 	private final String realName;
  
 	private TagPropertyName(String property) {
