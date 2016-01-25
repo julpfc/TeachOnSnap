98e3c311ad6404ff3b8ada877a4ac1b34b67cbea
julpfc <julpfc@users.noreply.github.com>
2016-01-25 17:41:31 +0100

    Javado tag prev

--- src/main/java/com/julvez/pfc/teachonsnap/tag/model/TagFollowed.java
+++ src/main/java/com/julvez/pfc/teachonsnap/tag/model/TagFollowed.java
@@ -1,10 +1,18 @@
 package com.julvez.pfc.teachonsnap.tag.model;
 
+/**
+ * Domain entity. Describes a followable tag, subtype of Tag 
+ * that can be followed by users or groups.
+ */
 public class TagFollowed extends Tag {
 
+	/** Indicates if the tag is followed */
 	private boolean followed;
 	
-	
+	/**
+	 * Constructs an TagFollowed object from a Tag object
+	 * @param tag which TagFollowed is copied from.
+	 */
 	public TagFollowed(Tag tag) {	
 		super();
 		setId(tag.getId());
@@ -12,12 +20,19 @@ public class TagFollowed extends Tag {
 		setMD5(tag.getMD5());
 	}
 
+	/**
+	 * @return if the tag is followed
+	 */
 	public boolean isFollowed() {
 		return followed;
 	}
 
+	/**
+	 * Sets if the tag is followed
+	 * @param followed or not
+	 */
 	public void setFollowed(boolean followed) {
 		this.followed = followed;
-	}	
+	}
 	
 }
