f221f6167c5cc96578b76ec6eae30f448f3b5ddd
julpfc <julpfc@users.noreply.github.com>
2016-01-25 17:37:51 +0100

    Javadoc tag prev

--- src/main/java/com/julvez/pfc/teachonsnap/tag/model/Tag.java
+++ src/main/java/com/julvez/pfc/teachonsnap/tag/model/Tag.java
@@ -5,16 +5,27 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
+/**
+ * Entity. Describes a lesson tag so the lesson can be searched by. 
+ * It contains the tag text, an MD5 hash and an URL pointing to the lessons. 
+ * tagged with it.
+ */
 @Entity
 public class Tag {
 	
+	/** Tag identifier and primary key for the entity */
 	@Id
 	@Column (name="idTag")
 	private int id;
+	
+	/** Tag text */
 	private String tag;
+	
+	/** Tag's MD5 hash */
 	@Transient
 	private String MD5;
 	
+	/** Lessons tagged with this tag's URL*/
 	@Transient
 	private String url;
 
@@ -23,36 +34,47 @@ public class Tag {
 		return "Tag [id=" + id + ", tag=" + tag + "]";
 	}
 
+	/**
+	 * @return tag's id
+	 */
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
+	/**
+	 * @return tag's text
+	 */
 	public String getTag() {
 		return tag;
 	}
 
-	public void setTag(String tag) {
-		this.tag = tag;
-	}	
-	
+	/**
+	 * Sets tag's URL to access tagged lessons
+	 * @param url tagged lessons URL
+	 */
 	public void setURLs(String url){
 		this.url = url;
 	}
 	
+	/**
+	 * @return tag's URL pointing to the tagged leesons
+	 */
 	public String getURL() {
 		return url + tag;
 	}
 	
+	/**
+	 * Sets tag's MD5 hash
+	 * @param mD5 hash
+	 */
 	public void setMD5(String md5) {
 		MD5 = md5;
 	}
 	
+	/**
+	 * @return tag's MD5 hash
+	 */
 	public String getMD5() {		
 		return MD5;
 	}
-	
 }
