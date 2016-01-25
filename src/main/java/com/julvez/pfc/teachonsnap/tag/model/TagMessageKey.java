47cd716e910e726ddcc15f151e17276f715e6219
julpfc <julpfc@users.noreply.github.com>
2016-01-25 18:02:24 +0100

    Javadoc tag prev

--- src/main/java/com/julvez/pfc/teachonsnap/tag/model/TagMessageKey.java
+++ src/main/java/com/julvez/pfc/teachonsnap/tag/model/TagMessageKey.java
@@ -1,10 +1,16 @@
 package com.julvez.pfc.teachonsnap.tag.model;
 
+/**
+ * Enumeration with the keys for localized messages related with tags
+ */
 public enum TagMessageKey {
+	/** Subject to notify a lesson was tagged with a followed tag */
 	LESSON_TAGGED_SUBJECT("lesson.tagged.subject"), 
+	
+	/** Message body to notify a lesson was tagged with a followed tag */
 	LESSON_TAGGED_MESSAGE("lesson.tagged.message");
 		
-	
+	/** Real message key on the messages properties file */
 	private final String realName;
  
 	private TagMessageKey(String property) {
