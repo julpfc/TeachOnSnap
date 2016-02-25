79d6b726978dd73a7852535cace3de3b733b90f3
julpfc <david.julvez@gmail.com>
2016-02-25 17:15:13 +0100

    ut: manager.log

--- src/test/java/com/julvez/pfc/teachonsnap/ut/manager/log/LogManagerTest.java
+++ src/test/java/com/julvez/pfc/teachonsnap/ut/manager/log/LogManagerTest.java
@@ -1,42 +1,24 @@
 package com.julvez.pfc.teachonsnap.ut.manager.log;
 
-import java.io.PrintStream;
-
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.julvez.pfc.teachonsnap.manager.log.LogManager;
 import com.julvez.pfc.teachonsnap.ut.manager.ManagerTest;
 
-public abstract class LogManagerTest extends ManagerTest<LogManager> {
 
-	static PrintStream out;
-	
-	@BeforeClass
-	public static void setUpClass() {		
-		out = Mockito.spy(System.out);
-		System.setOut(out);
-	}
+public abstract class LogManagerTest extends ManagerTest<LogManager> {
 
 	@Test
 	public void testDebug() {
-		String message = "debug";
-		String messageAndCR = message + "\r\n";
-
+		String message = "debug";	
 		test.debug(message);
-	
-		Mockito.verify(out, Mockito.atLeastOnce()).write(messageAndCR.getBytes(),0, messageAndCR.getBytes().length);		
 	}
 
 	@Test
 	public void testErrorString() {
 		String message = "error";
-		String messageAndCR = message + "\r\n";
-
 		test.error(message);
-		
-		Mockito.verify(out, Mockito.atLeastOnce()).write(messageAndCR.getBytes(),0, messageAndCR.getBytes().length);
 	}
 
 	@Test
@@ -46,64 +28,46 @@ public abstract class LogManagerTest extends ManagerTest<LogManager> {
 		Mockito.when(t.getStackTrace()).thenReturn(new StackTraceElement[0]);
 
 		test.error(t, message);
-		
-		Mockito.verify(out, Mockito.atLeastOnce()).write((byte[])Mockito.anyObject(),Mockito.anyInt(), Mockito.anyInt());
 	}
 
 	@Test
 	public void testInfo() {
 		String message = "info";
-		String messageAndCR = message + "\r\n";
-
 		test.info(message);
-		
-		Mockito.verify(out, Mockito.atLeastOnce()).write(messageAndCR.getBytes(),0, messageAndCR.getBytes().length);
 	}
 
 	@Test
 	public void testAddPrefix() {
 		String prefix = "prefix";
 		String message = "prefixTest";
-		String messageAndCR = message + "\r\n";
-
 		test.addPrefix(prefix);		
 		test.info(message);
-		
-		Mockito.verify(out, Mockito.atLeastOnce()).write(messageAndCR.getBytes(),0, messageAndCR.getBytes().length);
 	}
 
 	@Test
 	public void testRemovePrefix() {
 		String prefix = "prefix";
 		String message = "noPrefixTest";
-		String messageAndCR = message + "\r\n";
 
 		test.addPrefix(prefix);		
-		test.info(message);
 		test.removePrefix();		
-		Mockito.verify(out, Mockito.atLeastOnce()).write(messageAndCR.getBytes(),0, messageAndCR.getBytes().length);
+		test.info(message);
 	}
 
 	@Test
 	public void testTimer() {
 		String message = "timerTest";
-		String messageAndCR = "[00:00:000] " + message + "\r\n";
 
 		test.startTimer();		
 		test.infoTime(message);
-		
-		Mockito.verify(out, Mockito.atLeastOnce()).write((byte[])Mockito.anyObject(),Mockito.eq(0), Mockito.eq(messageAndCR.getBytes().length));
 	}
 
 	@Test
 	public void testClearTimer() {
 		String message = "clearTimerTest";
-		String messageAndCR = message + "\r\n";
-
 		test.startTimer();		
 		test.clearTimer();
 		test.info(message);
-		Mockito.verify(out, Mockito.atLeastOnce()).write(messageAndCR.getBytes(),0, messageAndCR.getBytes().length);
 	}
 
 }
--- src/test/java/com/julvez/pfc/teachonsnap/ut/manager/log/log4j2/LogManagerLog4j2Test.java
+++ src/test/java/com/julvez/pfc/teachonsnap/ut/manager/log/log4j2/LogManagerLog4j2Test.java
@@ -1,13 +1,106 @@
 package com.julvez.pfc.teachonsnap.ut.manager.log.log4j2;
 
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
 import com.julvez.pfc.teachonsnap.manager.log.LogManager;
 import com.julvez.pfc.teachonsnap.manager.log.log4j2.LogManagerLog4j2;
 import com.julvez.pfc.teachonsnap.ut.manager.log.LogManagerTest;
 
 public class LogManagerLog4j2Test extends LogManagerTest {
 
+	static Appender app;
+	
 	@Override
 	protected LogManager getManager() {
 		return new LogManagerLog4j2();
-	}	
+	}
+	
+	@BeforeClass
+	public static void setUpClass(){
+		Logger logger = (Logger) org.apache.logging.log4j.LogManager.getLogger(LogManagerTest.class.getName());
+		app = Mockito.mock(Appender.class); 
+		Mockito.when(app.getName()).thenReturn("MockAppender");
+		Mockito.when(app.isStarted()).thenReturn(true);
+		Mockito.when(app.isStopped()).thenReturn(false);
+		logger.addAppender(app);
+	}
+	
+	@Test
+	public void testDebug() {
+		super.testDebug();
+		ArgumentCaptor<LogEvent> captor = ArgumentCaptor.forClass(LogEvent.class);
+		Mockito.verify(app, Mockito.atLeastOnce()).append(captor.capture());
+		Assert.assertEquals("debug", captor.getValue().getMessage().getFormattedMessage());
+		Assert.assertEquals("DEBUG", captor.getValue().getLevel().name());
+	}
+
+	@Override
+	public void testErrorString() {
+		super.testErrorString();
+		ArgumentCaptor<LogEvent> captor = ArgumentCaptor.forClass(LogEvent.class);
+		Mockito.verify(app, Mockito.atLeastOnce()).append(captor.capture());
+		Assert.assertEquals("error", captor.getValue().getMessage().getFormattedMessage());
+		Assert.assertEquals("ERROR", captor.getValue().getLevel().name());
+	}
+
+	@Override
+	public void testErrorThrowableString() {
+		super.testErrorThrowableString();
+		ArgumentCaptor<LogEvent> captor = ArgumentCaptor.forClass(LogEvent.class);
+		Mockito.verify(app, Mockito.atLeastOnce()).append(captor.capture());
+		Assert.assertEquals("exception", captor.getValue().getMessage().getFormattedMessage());
+		Assert.assertEquals("ERROR", captor.getValue().getLevel().name());
+	}
+
+	@Override
+	public void testInfo() {
+		super.testInfo();
+		ArgumentCaptor<LogEvent> captor = ArgumentCaptor.forClass(LogEvent.class);
+		Mockito.verify(app, Mockito.atLeastOnce()).append(captor.capture());
+		Assert.assertEquals("info", captor.getValue().getMessage().getFormattedMessage());
+		Assert.assertEquals("INFO", captor.getValue().getLevel().name());
+	}
+
+	@Override
+	public void testAddPrefix() {
+		super.testAddPrefix();
+		ArgumentCaptor<LogEvent> captor = ArgumentCaptor.forClass(LogEvent.class);
+		Mockito.verify(app, Mockito.atLeastOnce()).append(captor.capture());
+		Assert.assertEquals("prefixTest", captor.getValue().getMessage().getFormattedMessage());
+		Assert.assertEquals("INFO", captor.getValue().getLevel().name());
+	}
+
+	@Override
+	public void testRemovePrefix() {
+		super.testRemovePrefix();
+		ArgumentCaptor<LogEvent> captor = ArgumentCaptor.forClass(LogEvent.class);
+		Mockito.verify(app, Mockito.atLeastOnce()).append(captor.capture());
+		Assert.assertEquals("noPrefixTest", captor.getValue().getMessage().getFormattedMessage());
+		Assert.assertEquals("INFO", captor.getValue().getLevel().name());
+	}
+
+	@Override
+	public void testTimer() {
+		super.testTimer();
+		ArgumentCaptor<LogEvent> captor = ArgumentCaptor.forClass(LogEvent.class);
+		Mockito.verify(app, Mockito.atLeastOnce()).append(captor.capture());
+		Assert.assertEquals("[00:00:000] timerTest", captor.getValue().getMessage().getFormattedMessage());
+		Assert.assertEquals("INFO", captor.getValue().getLevel().name());
+	}
+
+	@Override
+	public void testClearTimer() {
+		super.testClearTimer();
+		ArgumentCaptor<LogEvent> captor = ArgumentCaptor.forClass(LogEvent.class);
+		Mockito.verify(app, Mockito.atLeastOnce()).append(captor.capture());
+		Assert.assertEquals("clearTimerTest", captor.getValue().getMessage().getFormattedMessage());
+		Assert.assertEquals("INFO", captor.getValue().getLevel().name());
+	}
 }
